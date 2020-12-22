2eb8d5c808a380189892aca1340a9c9ce6fe64a1
Tamal Sarkar <tamalsarkar@Tamals-MacBook-Pro.local>
2020-12-22 14:37:56 -0500

    Updated to handle parameters as python dictionaries

--- Framework/Create_WF.py
+++ Framework/Create_WF.py
@@ -3,7 +3,7 @@ import json
 import logging
 import sys
 
-import pyspark
+from pyspark import SparkConf,SparkContext
 from pyspark.sql import SQLContext
 
 import inputTemplate
@@ -11,6 +11,11 @@ import outputTemplate
 
 
 def logHandler(wfJSON):
+    """
+    This function Creates the log handler by extracting the log path from workflow json
+    :param wfJSON: Input Json
+    :return: log handler context
+    """
     curr_timestamp = datetime.datetime.now().strftime("%Y%m%d_%I%M%s")
     logname = wfJSON['LOG_LOCATION']+'_' +curr_timestamp +'.log'
     logger =logging.getLogger()
@@ -23,11 +28,12 @@ def logHandler(wfJSON):
 
     return logger
 
-
 def readJson(wfJSON):
     """
-    :param file:
-    :return:
+    This function is used to orchestrate the execution of the process by parsing the workflow json and creating the
+    workflow steps in sqlcontext
+    :param file: workflow json
+    :return: Successfully completed Task Project Name
     """
 
     TASK_PROJECT= wfJSON['TASK_PROJECT']
@@ -37,11 +43,12 @@ def readJson(wfJSON):
         for steps in range(len(wfSteps)):
             stepVal=wfSteps[steps]
             logger.info(f'Current Step {stepVal}')
-            if stepVal['TASK_KEY'] == 'input':
-                filePath=stepVal['TASK_VALUE']['PARAM'][-1][5:]
-                logger.info(f'Input File Path :{filePath}')
+            if stepVal['TASK_CATEGORY'] == 'input':
+                filePath=stepVal['TASK_VALUE']['PARAM']
+                fileType=stepVal['TASK_VALUE']['TYPE']
+                logger.info(f'Input File Path with Arguments :{filePath} and file type is {fileType}')
                 try:
-                    dF1=inputTemplate.ReadFile.read_file(filePath)
+                    dF1=inputTemplate.read_file(filePath,fileType)
                 except Exception as e :
                     logger.error(e)
                     exit(1)
@@ -49,7 +56,7 @@ def readJson(wfJSON):
                     stepVal['TASK_NAME']=dF1.createOrReplaceTempView(stepVal['TASK_NAME'])
                     logger.info(f'Successfully registered :{filePath} as a Temp Table')
                     logger.info("===========================================================")
-            elif stepVal['TASK_KEY'] == 'transform':
+            elif stepVal['TASK_CATEGORY'] == 'transform':
                 transformOp = stepVal['TASK_VALUE']['PARAM'][0]
                 logger.info(f'Transformation : {transformOp}')
                 try:
@@ -67,17 +74,19 @@ def readJson(wfJSON):
                     logger.error(e)
                     exit(3)
                 else:
-                    logger.info(f"Successfully Registered temp Table")
+
+                    logger.info(f'Successfully Registered temp Table')
                     logger.info("===========================================================")
-            elif stepVal['TASK_KEY'] == 'output':
-                outfilePath = stepVal['TASK_VALUE']['PARAM'][-1][5:]
-                logger.info(f'Output File Path :{outfilePath}')
+            elif stepVal['TASK_CATEGORY'] == 'output':
+                outfilePath = stepVal['TASK_VALUE']['PARAM']
                 outputType = stepVal['TASK_VALUE']['TYPE']
-                logger.info(f'Output FileType: {outputType}')
                 inputDS = stepVal['TASK_VALUE']['INPUT']
-                logger.info(f'File to be writen: {inputDS}')
+                logger.info(f'Output File Path :{outfilePath}')
+                logger.info(f'Output FileType is {outputType} and dataframe to be writen is {inputDS}')
+                logger.info(f'{outfilePath[0]["Key"]} : {outfilePath[0]["value"]} ')
+                logger.info(f'{outfilePath[1]["Key"]} : {outfilePath[1]["value"]} ')
                 try:
-                    outputTemplate.WriteFile.write_file(dF2,dF2,outputType,outfilePath)
+                    outputTemplate.write_file(outfilePath,outputType,inputDS)
                 except Exception as e:
                     logger.error(e)
                     exit(4)
@@ -85,7 +94,8 @@ def readJson(wfJSON):
                     logger.info(f'Output has been writen to {outfilePath}')
                     logger.info("===========================================================")
             else:
-                """default msg"""
+                logger.info("========== WORKING ON THIS FEATURE ==============")
+                exit(404)
 
     return TASK_PROJECT
 
@@ -93,20 +103,19 @@ def main():
     """
     The main function is used to trigger the parsing of the input workflow json and make calls to function modules
     as defined in the JSON
-    :return: WorkFlow status for TASK PRJECT
+    :return: WorkFlow status for TASK PROJECT
     """
     try:
-        print('Submitted Workflow JSON for processing. Please refer to log file for additional details')
+        logger.info('Submitted Workflow JSON for processing. Please refer to log file for additional details')
         WF_NAME=readJson(wfJSON)
     except Exception as e:
-        print(f'Please Correct the Input JSON and resubmit. Detailed Error Message below:'
+        logger.error(f'Please Correct the Input JSON and resubmit. Detailed Error Message below:'
               f'{e}')
     else:
-        print('Successfully Completed Workflow. Please refer to log file for additional details')
+        logger.info('Successfully Completed Workflow. Please refer to log file for additional details')
 
 if __name__ == "__main__":
-    conf = pyspark.SparkConf()
-    sc = pyspark.SparkContext.getOrCreate(conf=conf)
+    sc = SparkContext.getOrCreate(conf=SparkConf())
     spark = SQLContext(sc)
     try:
         with open(sys.argv[1]) as inputJson:
--- Framework/inputTemplate.py
+++ Framework/inputTemplate.py
@@ -3,40 +3,23 @@ import os
 
 spark = SparkSession.builder.appName('WorkflowApp').getOrCreate()
 
-class ReadFile(object):
+def read_file(paramList,fileType):
     """
-    Class for defining various file reading options
+    Called with the Param List with options based on file type
+    CSV : Default encoding utf-8,Header=True,InferSchema=True
+    Parquet : Default configuration
+    :return:
     """
-    def __init__(self,Name):
-        self.Name = Name
-        print(Name)
-
-    def extn(self):
-        filename,file_extension = os.path.splitext(self)
-        print(f'Extension is {file_extension}')
-        return file_extension
-    def read_file(self):
-        """
-        Called with the Self parameter of input file path
-        CSV : Default encoding utf-8,Header=True,InferSchema=True
-        Parquet : Default configuration
-        :return:
-        """
-        filename,file_extension = os.path.splitext(self)
-        if file_extension == ".csv":
-            df = spark.read.csv(self,encoding=None,header=True, inferSchema=True)
-        elif file_extension == ".json":
-            df = spark.read.json(self)
-        elif file_extension == ".parquet":
-            df = spark.read.load(self)
+    fileType=fileType.upper()
+    if paramList[0]['Key'] == 'path':
+        if fileType == "CSV" or fileType == "DAT":
+            df = spark.read.load(paramList[0]['value'],format='csv',header=True,sep=paramList[1]['value'],inferSchema=True)
+        elif fileType == "JSON":
+            df = spark.read.load(paramList[0]['value'],format='json')
+        elif fileType == "PARQUET":
+            df = spark.read.load(paramList[0]['value'])
         else:
             print("File Extension provided is not yet supported")
-        return df
+            exit(11)
+    return df
 
-    def read_file_custom_header(self,column_names,skip):
-        filename,file_extension = os.path.splitext(self)
-        if file_extension == ".xlsx":
-            filename.pandas.read_excel(open(self,'rb'),skiprows=skip,names=column_names)
-        elif file_extension == ".csv":
-            filename.pandas.read_csv(open(self,'rb'),skiprows=skip,names=column_names)
-        return filename
