e54cf13066953fdf1a23debb0c9480b649c6e51d
Tamal Sarkar <tamalsarkar@Tamals-MacBook-Pro.local>
2021-02-17 17:27:52 -0500

    Added support for Fixed length file

--- Framework/inputTemplate.py
+++ Framework/inputTemplate.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
-import os
+import pandas as pd
+import ast
 
 spark = SparkSession.builder.getOrCreate()
 
@@ -18,6 +19,27 @@ def read_file(paramList,fileType):
             df = spark.read.load(paramList[0]['value'],format='json')
         elif fileType == "PARQUET":
             df = spark.read.load(paramList[0]['value'])
+        elif fileType == "FIXED":
+            try:
+                paramList[1]['Key']== "ColumnSpecification"
+            except Exception as e:
+                print(e)
+                exit(110)
+            else:
+                col_spec=list(ast.literal_eval(paramList[1]['value']))
+                print(col_spec)
+
+            try:
+                paramList[2]['Key'] == "columnName"
+            except Exception as e:
+                print(e)
+                print("Custom column names not provided")
+            else:
+                columnName = paramList[2]['value'].split(",")
+                print(columnName)
+                data=pd.read_fwf(paramList[0]['value'],colspecs=col_spec,header=None,names=columnName)
+                df = spark.createDataFrame(data.astype(str))
+
         else:
             print("File Extension provided is not yet supported")
             exit(11)
